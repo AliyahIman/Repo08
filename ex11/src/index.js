7fd199ce3cbfce1ecdffe55b729b7513e272ae2f
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-21 06:34:41 +0200

    I give up

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,10 @@
+// Only change code below this line
 
+function getRndInteger(min, max) {
+
+    return Math.floor(Math.random() * (30 - 0 + 1) ) + min;
+
+}
+
+// Only change code above this line
+module.exports = randomInteger;
