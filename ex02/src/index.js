8b600060b22938916fbb98f32d3a0a84503d3f40
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-21 05:45:07 +0200

    added another index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,12 @@
+// Write code below this line
 
+function functionPrintInfo(name, age){
+    console.log(name, age);
+}
+
+console.log(functionPrintInfo("John", 23));
+console.dir(functionPrintInfo("Micheal", 43));
+
+// Write code above this line
+
+module.exports = functionPrintInfo;
