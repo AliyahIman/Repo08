3ea6ad4efc1703d3bd41ef0931d1a436352b7b4b
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-21 05:44:24 +0200

    Added index.js ;)

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,11 @@
+// Write code below this line
 
+function reusableFunction(){
+    console.log("Hi Padawan!");
+}
+
+console.log(reusableFunction());
+
+// Write code above this line
+
+module.exports = reusableFunction;
